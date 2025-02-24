8711e722c14132a06c6061fe8f51904a8a420e2c
natalie363 <natalie.eckles@mymail.champlain.edu>
2025-02-23 20:21:43 -0500

    Testing SSH Key

--- milestone4.ps1
+++ milestone4.ps1
@@ -1,4 +1,4 @@
-get-vm
+get-folder "Base VMs" | get-vm
 Write-Host ""
 $selectedVM = Read-Host "Enter the name of the VM that should be cloned"
 $newName = Read-Host "Enter the new VM's name"
@@ -8,16 +8,17 @@ $snapshot = Get-Snapshot -vm $vm -Name "base"
 $vmhost = Get-VMHost -Name "super3.eckles.local"
 $ds = Get-Datastore -Name "datastore1"
 If ($cloneType -eq "F"){
-        $linkedname = "{0}.linked" -f $vm.name
-        $linkedvm = New-VM -LinkedClone -Name $linkedName -VM $vm -ReferenceSnapshot $sna>
-        $newvm = New-VM -Name $newName -VM $linkedvm -VMHost $vmhost -Datastore $ds
-        $newvm | new-snapshot -Name "base"
-        $linkedvm | Remove-VM -Confirm:$false
-        }
+	$linkedname = "{0}.linked" -f $vm.name
+	$linkedvm = New-VM -LinkedClone -Name $linkedName -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds
+	$newvm = New-VM -Name $newName -VM $linkedvm -VMHost $vmhost -Datastore $ds
+	$newvm | new-snapshot -Name "base"
+	$linkedvm | Remove-VM -Confirm:$false
+	}
 Elseif ($cloneType -eq "L"){
-        $linkedvm = New-VM -LinkedClone -Name $newName -VM $vm -ReferenceSnapshot $snapsh>
-        $linkedvm | new-snapshot -Name "base"
-        }
+	$linkedvm = New-VM -LinkedClone -Name $newName -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds
+	$linkedvm | new-snapshot -Name "base"
+	}
 else {
-        write-host "Invalid selection, program ending"
-        }
+	write-host "Invalid selection, program ending"
+	
+	}
